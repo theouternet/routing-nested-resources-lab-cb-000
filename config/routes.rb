bb59b20f845728c2cecdbc98feda1c4ad0d4fdb2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-04 04:56:32 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:show] do
+  resources :artists, do
 
     resources :songs, only: [:index, :show]
 end 
 
- resources :songs, only: [:index, :show, :new, :create, :edit, :update]
- 
-  root 'posts#index'
-  
+ resources :songs
+
 end 
 
-Rails.application.routes.draw do
-  resources :artists
-  resources :artists do
-    resources :songs, only: [:index, :show]
-  end
-  resources :songs
-  resources :songs
-end
-end
